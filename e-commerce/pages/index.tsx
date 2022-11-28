870ab93b0627cdf3317f655c38d3e87c946d7a0d
Ade Tola <adetolaisaiah17@gmail.com>
2022-11-28 10:31:32 +0100

    first commit

---
+++ e-commerce/pages/index.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import styled from 'styled-components'
+import Image from 'next/image'
+import Navbar from '../components/Navbar/Navbar'
+import Categories from '../components/Categories/Categories'
+
+
+
+const Main = styled.div`
+    
+`
+export default function Home() {
+  return (
+    <>
+
+      <Navbar/>
+
+      <Main>
+
+      <Categories/>
+
+     
+    </Main>
+    </>
+  )
+}
